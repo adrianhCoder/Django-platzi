fba66405a8b76daf692f0c9ef7ca6d28bf84d94c
Adrianh <adrianh_coder@outlook.com>
2023-06-14 15:13:52 -0500

    Agregamos 4 vistas

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("You are in the main page of premiosplatziapp")
+
 
 # Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero: {question_id}")
+
