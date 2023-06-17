51acfa1160a64ba0dfdd90cd2274d9f269dcb84b
Adrianh <adrianh_coder@outlook.com>
2023-06-17 14:49:32 -0500

    Agregamos views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,16 +1,23 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html",{"latest_question_list" : latest_question_list})
+    return render(request, "polls/index.html",{
+        "latest_question_list" : latest_question_list
+        })
+
 
 
 # Create your views here.
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, "polls/detail.html",{
+        "question" : question
+        })
+
 
 
 def results(request, question_id):
