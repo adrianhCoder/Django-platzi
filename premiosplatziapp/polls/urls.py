8c1dfb0013fd3ca771fe3041737fb83f34dce413
Adrianh <adrianh_coder@outlook.com>
2023-06-30 18:49:31 -0500

    No arranca la aplicacion

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -5,12 +5,12 @@ app_name = "polls"
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     # ex: /polls/18
-    path("<int:question_id>/detail/cambiandotodoojaja", views.detail, name="detail"),
+    path("<int:pk>/detail/cambiandotodoojaja", views.DetailView.as_view(), name="detail"),
     # ex: /polls/18/results
-    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:pk>/results", views.ResultView.as_view(), name="results"),
     # ex: /polls/18/vote
-    path("<int:question_id>/vote", views.vote, name="vote")
+    path("<int:pk>/vote", views.vote, name="vote")
 
 ]
