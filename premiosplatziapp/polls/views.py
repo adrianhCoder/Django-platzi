ffdd78b492dd1286ada9ca4306c9cdf776e4f048
Adrianh <adrianh_coder@outlook.com>
2023-07-18 11:35:35 -0600

    Delete unecesaru commented code

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,32 +3,7 @@ from .models import Question, Choice
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-
-
-# def index(request):
-#     latest_question_list = Question.objects.all()
-#     return render(request, "polls/index.html",{
-#         "latest_question_list" : latest_question_list
-#         })
-
-
-
-# # Create your views here.
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question,pk=question_id)
-#     return render(request, "polls/detail.html",{
-#         "question" : question
-#         })
-
-
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question,pk=question_id)    
-#     return render(request,"polls/results.html", {
-#         "question" : question
-#     })
-
+from django.utils import  timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
