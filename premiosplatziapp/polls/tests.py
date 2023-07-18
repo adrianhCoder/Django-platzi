25832ca1e3a0fa79f2d0473088dba849ec7e61c5
Adrianh <adrianh_coder@outlook.com>
2023-07-18 11:05:30 -0600

    Cration of first test to question model pub date

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
+from .models import Question
+
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """Was published recentlly returns
+        false for questions whos pub date is in the fytyre"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text='Helloy',pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+        
