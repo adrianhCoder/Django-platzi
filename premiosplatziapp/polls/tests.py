39d1f4e70bae74f60cd0c00441ac7769ecbb1ec3
Adrianh <adrianh_coder@outlook.com>
2023-07-18 11:21:05 -0600

    2 more test aded for questions

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -15,4 +15,16 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text='Helloy',pub_date=time)
         self.assertIs(future_question.was_published_recently(),False)
-        
+
+
+    def test_was_published_in_the_past(self):
+        """ Checks if an answer was published in the past"""
+        time = timezone.now() - datetime.timedelta(days=30)
+        past_question = Question(question_text="Question text here", pub_date=time)
+        self.assertIs(past_question.was_published_recently(),False)
+
+    def test_was_published_recently_to_timenow_questions(self):
+        """Checks if question created now is recent"""
+        time = timezone.now()
+        now_question = Question(question_text="Question text here", pub_date = time)
+        self.assertIs(now_question.was_published_recently(),True)
